1d92cca41f4a8696449c7f125dc6f7f04e5adb0a
Natan <natancmendes@gmail.com>
2022-05-04 11:01:32 -0300

    tela gerenciamento de conta (b.d. funciona)

--- php/rotinas/altera.php
+++ php/rotinas/altera.php
@@ -1,24 +1,31 @@
 <?php
-    $conexao = mysqli_connect("localhost", "root", "", "Vienna2");
+    $conexao = mysqli_connect("localhost", "root", "", "Vienna2"); 
 
     //revive a sessao do usuario
     if(!isset($_SESSION)){
         session_start();
     }
 
-    if(isset($_POST["nome"]) && isset($_POST["email"]) && isset($_POST["senha"]) && isset($_POST["confSenha"])){
+    if(isset($_POST["nome"]) && isset($_POST["email"]) && isset($_POST["senha"]) && isset($_POST["confSenha"]) && $_POST["senha"] == $_POST["confSenha"]){
+        $id = $_POST['id'];
         $nome_novo = $_POST['nome'];
         $email_novo = $_POST['email'];
         $senha_novo = $_POST['senha'];
 
         $hash_senha = hash("sha256", $senha_novo, false);
-        $sql = "UPDATE `usuarios` SET  `nome`='$nome_novo', `email`='$email_novo', `senha`='$hash_senha' WHERE `email`='$email_novo'";
-        $executa= mysqli_query($conexao,$sql);
+        
+        try{
+            $sql = "UPDATE `usuarios` SET  `nome`='$nome_novo', `email`='$email_novo', `senha`='$hash_senha' WHERE `id`='$id'";
+            $executa= mysqli_query($conexao,$sql);
+        } catch (mysqli_sql_exception $e){
+            die("Ocorreu um erro na atualização do usuário. ({$e->getMessage()})");
+        }        
 
         if($executa)
 		{
 			session_destroy();
             echo "<div style='color:#F00'> Sucesso ao atualizar</div><br/><br/>";
+            header("location: ../../html/home.php");
 		}else{
 			echo "<div style='color:#F00'> Erro ao atualizar</div><br/><br/>";
 		}
